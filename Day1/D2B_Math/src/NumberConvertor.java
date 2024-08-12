42e993faa7a9c3d43d8b68df3cf12cb9114803e5
Rohan Gorakhnath Chavan <rohangchavan@gmail.com>
2024-08-12 12:12:49 +0530

    Not Needed

--- Day1/D2B_Math/src/NumberConvertor.java
+++
@@ -1,6 +0,0 @@
-public class NumberConvertor {
-    public static void main(String[] args) {
-        DecimalToBinaryConverter numberOne=new DecimalToBinaryConverter();
-        System.out.println("Binary number of 10"+" is "+numberOne.convertToBinary(10));
-    }
-}
--- Day1/Factorial/src/CalculateFactorial.java
+++
@@ -1,7 +0,0 @@
-public class CalculateFactorial {
-    public static void main(String[] args) {
-        Factorial factorialOne=new Factorial();
-        System.out.println("By Normal For Loop: "+factorialOne.getFactorial(9));
-        System.out.println("By Recursion :"+factorialOne.getFactorialByRecursion(9));
-    }
-}
